64e8e3ab0467da4ba96de0c126d8d81b5b9037df
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-10 23:37:26 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ new Vue({
             
             if(blan == 'Januari'){
                 return this.watak = `
-                ·Tenang dan berwibawa
+                  ·Tenang dan berwibawa</br>
                   ·Suka berterus terang dan tidak suka basa-basi (to the point)
                   ·pandai menyimpan rahasia dan bisa dipercaya (ini yang bagus)
                   ·Disukai banyak orang karena selalu kelihatan ceria (yang penting nda ketawa-ketawa sendiri)
